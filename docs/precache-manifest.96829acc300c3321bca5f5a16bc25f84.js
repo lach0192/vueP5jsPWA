542ebf35ad5a1b46aa9b2483e02ba2bc1eef713a
Eric Lachapelle <elachapelle@mcmillan.com>
2019-02-19 11:09:52 -0500

    Update

---
+++ docs/precache-manifest.96829acc300c3321bca5f5a16bc25f84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "170821e7a72cf8c1c58e451e35c362a9",
+    "url": "/vueP5jsPWA/service-worker.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vueP5jsPWA/robots.txt"
+  },
+  {
+    "revision": "71cbe76df1fe7601ea96",
+    "url": "/vueP5jsPWA/js/chunk-vendors.a81164a3.js"
+  },
+  {
+    "revision": "61ed0016ac31db4c52f9",
+    "url": "/vueP5jsPWA/js/app.99be18f4.js"
+  },
+  {
+    "revision": "118d5267bb7cde3057ca",
+    "url": "/vueP5jsPWA/js/about.aeb9e32a.js"
+  },
+  {
+    "revision": "6fcd97f64e0f4b3cbc5927bf7552635d",
+    "url": "/vueP5jsPWA/index.html"
+  },
+  {
+    "revision": "61ed0016ac31db4c52f9",
+    "url": "/vueP5jsPWA/css/app.b1f6641b.css"
+  },
+  {
+    "revision": "118d5267bb7cde3057ca",
+    "url": "/vueP5jsPWA/css/about.5af527fd.css"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/vueP5jsPWA/precache-manifest.38114457870b62b9e832699292846d4f.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/vueP5jsPWA/precache-manifest.96829acc300c3321bca5f5a16bc25f84.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if (workbox) {
     console.log(`Workbox is loaded`);
