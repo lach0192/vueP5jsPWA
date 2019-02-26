ddeb75a35b8a8bc98b03b85378157297d660109a
Eric Lachapelle <elachapelle@mcmillan.com>
2019-02-26 13:37:01 -0500

    Update

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/vueP5jsPWA/precache-manifest.6068b21fb8a03f1c6ab5e23cce24dd8d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/vueP5jsPWA/precache-manifest.83f4099ffbb1ca7418b86c663866b8dc.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if (workbox) {
     console.log(`Workbox is loaded`);
